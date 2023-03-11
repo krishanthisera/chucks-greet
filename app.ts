518bb2355722dfb0d9bd98c33c7f1739f4d2192b
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-03-11 16:22:24 +1030

    Initial commit

---
+++ app.ts
@@ -0,0 +1,7 @@
+const message: string = `Hi from above ==> &#9981;`;
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
+
