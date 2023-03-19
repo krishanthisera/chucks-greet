5c61475c7aae90fbb542e8a00848bf15b0ba9381
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-03-19 16:25:56 +1030

    Update deployment configs

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
+$CODEDEPLOY_BIN stop
+yum erase codedeploy-agent -y
+
+cd /home/ec2-user
 
 yum update -y
-yum install -y httpd ruby wget
+yum install -y httpd ruby wget unzip
 
-wget https://bucket-name.s3.region-identifier.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 # Stop HTTPD service
@@ -13,5 +18,3 @@ sudo systemctl stop httpd
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
 sudo service codedeploy-agent enable
-
-
